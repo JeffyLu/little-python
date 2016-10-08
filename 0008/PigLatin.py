461cfe92c8a2ab78050c699f96ff92b008ead3c2
hhhhh <a214121@126.com>
2016-10-08 21:35:55 +0800

    修正纯数字判断

--- 0008/PigLatin.py
+++ 0008/PigLatin.py
@@ -7,13 +7,20 @@ import string
     
 def main():
     alph = [i for i in 'aeiou']
-    word = input("input a word:")
+    word = str(input("input a word:"))
+    if not word.isalpha():
+        print("input another one!")
+        return
     if word[0].lower() not in alph:
         word = word[1:] + '-' + word[0] + 'ay'
         print(word)
     else:
         print('input another one!')
-        main()
+        return
 
 if __name__ == '__main__':
-    main()
+    while(True):
+        try:
+            main()
+        except:
+            break
