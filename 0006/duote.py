e0b8c02af011e0fcf98cded4f82b2eb0926c3551
JeffyLu <a214121@126.com>
2017-01-24 23:35:44 +0800

    重构并增加大小、人气等信息

--- 0006/duote.py
+++ 0006/duote.py
@@ -1,44 +1,60 @@
-# -*- coding:gbk -*-
-import re
-import urllib2
+#coding:utf-8
 
-class pachong:
-    def __init__(self):
-        self.pageIndex = 1
-        self.num = 1
-        self.user_agent = 'Mozilla/5.0 (compatible; MSIE 5.5; Windows NT)'
-        self.headers = { 'User-Agent' : self.user_agent }
-      #  self.softwaresname = []
-        
-    def getPage(self, pageIndex):
-        print 'page %d' % pageIndex
-        url = 'http://www.duote.com/sort/50_0_wdow_0_' + str(pageIndex) + '_.html'
-        request = urllib2.Request(url, headers = self.headers)
-        response = urllib2.urlopen(request)
-        pageCode = response.read()
-        return pageCode
-    
-    def getPageItems(self):
-        pageCode = self.getPage(self.pageIndex)
-        if self.pageIndex == 1:
-            pattern = re.compile(r'<span class="count">.*?[0-9]/([0-9]*[1-9][0-9]*)')
-            num = re.findall(pattern, pageCode)
-            self.num = int(num[0])
-            #print int(num[0])
-        self.pageIndex += 1
-        pattern = re.compile(r'<a onclick=.*?l211.*?title="(.*?)">.*?</a>')
-        title = re.findall(pattern, pageCode)
-        return title
-    
-    def getTitle(self):
-        f = open("software.txt", 'w+')
-        count = 1
-        while self.pageIndex <= self.num:
-            for i in self.getPageItems():
-                temp = str(count) + '��' + i + '\n'
-                f.write(temp)
-                count += 1
-        print 'over!'
-        f.close()
-spider = pachong()
-spider.getTitle()
+import re
+import urllib.request
+
+class DuoteSpyder:
+
+    def __init__(self, url):
+        self.url = url
+        self.pages = 0
+        self.apps = 0
+        self.status = ''
+        self.count_pages_and_apps()
+
+    def count_pages_and_apps(self):
+        response = urllib.request.urlopen(url % 1)
+        content = response.read().decode('gbk')
+        counts = re.findall(r'第\d/(.*?)页  共有：(.*?)条', content)
+        try:
+            self.pages, self.apps = map(int, counts[0])
+        except Exception as e:
+            print(e)
+
+        print('本站包含{}个软件, 共有{}页'.format(self.apps, self.pages))
+
+    def get_apps(self):
+        for page in range(1, self.pages+1):
+            try:
+                response = urllib.request.urlopen(url % page, timeout=10)
+                content = response.read().decode('gbk')
+                self.status = '连接成功!'
+            except:
+                self.status = '连接失败!'
+
+            info = self.__filter(content)
+            with open('duote.txt', 'a') as f:
+                for i in info:
+                    app = '{0[2]}\t{0[3]}\t{0[0]}\t{0[1]}'.format(i)
+                    #print(app)
+                    f.write(app + '\n')
+
+            print('爬取第{}页\t{}'.format(page, self.status))
+
+
+    def __filter(self, content):
+        pattern = re.compile(
+            r'''>(人气.*?)<.*?>(好评率.*?)<.*?title="(.*?)".*?>(大小.*?)<''',
+            re.S
+        )
+        info = re.findall(pattern, content)
+        return info
+
+    def run_spyder(self):
+        self.get_apps()
+
+if __name__ == '__main__':
+
+    url = 'http://www.duote.com/sort/50_0_wdow_0_%d_.html'
+    spyder = DuoteSpyder(url)
+    spyder.run_spyder()
