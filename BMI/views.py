b8433f883060052f5dd8a2cee832dfee3d3366eb
JeffyLu <a214121@126.com>
2017-02-07 13:56:32 +0800

    #1 单独线程绘图

--- BMI/views.py
+++ BMI/views.py
@@ -5,6 +5,9 @@ from .models import Ip, Info
 from .forms import BMIForm
 import pylab as pl
 import numpy as np
+import threading
+import time
+import os
 
 #配置中文字体
 import matplotlib as mpl
@@ -22,34 +25,31 @@ def get_ip(request):
     return ip
 
 
-#BMI计算视图
 def view_of_BMI(request):
     ip = get_ip(request)
     if request.method == 'POST':
         form = BMIForm(request.POST)
-
-        #非法用户
         try:
-            if Ip.objects.get(ip = ip).blackone:
+            ip = Ip.objects.get(ip = ip)
+            if ip.blackone:
                 return HttpResponse('非法用户:' + ip)
         except:
-            Ip.objects.create(ip = ip)
-
-        #提交表单获取干净数据
+            ip = Ip.objects.create(ip = ip)
         if form.is_valid():
             Info.objects.create(
-                ip = Ip(ip),
+                ip = ip,
                 age = form.clean_age(),
                 gender = form.cleaned_data['gender'],
                 height = form.clean_height(),
                 weight = form.clean_weight(),
             )
+            condition.acquire()
+            stat_thread.ip = ip
+            condition.notifyAll()
+            condition.release()
             return HttpResponseRedirect('/bmi')
-
     else:
         form = BMIForm()
-
-    #显示最近5条数据
     try:
         history_data = Info.objects.filter(ip = ip)
         history_data = history_data.order_by('-date')[:5]
@@ -63,136 +63,137 @@ def view_of_BMI(request):
     return render(request, 'BMI.html', context)
 
 
-#个人数据统计
-def stat_personal(ip):
-    filename = './commonstatic/statistics/my.jpg'
+class Statistics(threading.Thread):
 
-    #统计记录个数
-    try:
-        ip = Ip.objects.get(ip = ip)
-        items = ip.info_set.count()
-    except:
-        return 0
+    age_bmi_count = None
+    items = None
+    condition = threading.Condition()
 
-    #提取数据
-    my_info = Info.objects.filter(ip = ip).order_by('date')
-    dates = list(range(len(my_info)))
-    bmis = []
-    for info in my_info:
-        bmis.append(info.get_bmi())
+    def __init__(self):
+        super().__init__()
+        self.file_path = './commonstatic/statistics/'
+        if not os.path.exists('./commonstatic/statistics'):
+            os.mkdir('./commonstatic/statistics')
+            print('make dir statistics.')
+        self.ip = None
 
-    #构造图表
-    pl.figure('my', figsize = (5.2, 2.8), dpi = 100)
-    pl.plot(dates, bmis, '*-', color = '#20b2aa', linewidth = 1.5)
-    pl.ylabel(u'BMI值', fontproperties = zhfont)
-    pl.ylim(0.0, 50.0)
-    pl.savefig(filename)
-    pl.cla()
-    return items
+    def stat_personal(self):
+        if not os.path.exists(self.file_path + self.ip.ip):
+            os.mkdir(self.file_path + self.ip.ip)
+            print('make dir %s' % self.ip.ip)
+        try:
+            items = self.ip.info_set.count()
+        except:
+            return 0
+        my_info = Info.objects.filter(ip = self.ip).order_by('date')
+        dates = list(range(len(my_info)))
+        bmis = [info.get_bmi() for info in my_info]
+        pl.figure('my', figsize = (5.2, 2.8), dpi = 100)
+        pl.plot(dates, bmis, '*-', color = '#20b2aa', linewidth = 1.5)
+        pl.ylabel(u'BMI值', fontproperties = zhfont)
+        pl.ylim(0.0, 50.0)
+        pl.savefig(self.file_path + self.ip.ip + '/my.jpg')
+        pl.cla()
+        return items
+
+    def stat_all_by_age(self):
+        bmi_by_age = []
+        sum_of_bmi = 0
+        count = 0
+        for i in range(1, 151):
+            infos = Info.objects.filter(age = i)
+            count += len(infos)
+            if infos:
+                for info in infos:
+                    sum_of_bmi += info.get_bmi()
+            else:
+                sum_of_bmi += 0
+            if not i%10:
+                bmi_by_age.append((i, sum_of_bmi, count))
+                sum_of_bmi = 0
+                count = 0
+        ages = [i for i in range(10, 151, 10)]
+        avebmi = [0 if i[1] == 0 else round(i[1]/i[2], 1) for i in bmi_by_age]
+        pl.figure('all0', figsize = (5.2, 2.8), dpi = 100)
+        pl.xlabel(u'年龄段', fontproperties = zhfont)
+        pl.ylabel('平均BMI', fontproperties = zhfont)
+        pl.ylim(0.0, 50.0)
+        pl.plot(ages, avebmi, '*', color = '#20b2aa')
+        pl.savefig(self.file_path + 'all0.jpg')
+        pl.cla()
+        avecount = [i[2] for i in bmi_by_age]
+        ages = ['%d-%d'%(i-9, i) for i in ages]
+        return list(zip(ages, avebmi, avecount))
+
+    def stat_all_by_rank(self):
+        ranks = ['偏瘦', '正常', '偏胖', '肥胖', '重度肥胖', '极度肥胖']
+        men_infos = Info.objects.filter(gender = '男')
+        women_infos = Info.objects.filter(gender = '女')
+        men_ranks = [i.get_rank() for i in men_infos]
+        women_ranks = [i.get_rank() for i in women_infos]
+        men_count = [men_ranks.count(r) for r in ranks]
+        women_count = [women_ranks.count(r) for r in ranks]
+        ind = np.arange(6)
+        width = 0.35
+        fig, ax = pl.subplots(figsize = (5.2, 2.8))
+        rects1 = ax.bar(ind, men_count, width, color = '#20b2aa')
+        rects2 = ax.bar(ind + width, women_count, width, color = 'w')
+        ax.set_ylabel('数量', fontproperties = zhfont)
+        ax.set_xlim(-0.5, 7)
+        ax.set_xticks(ind+width)
+        ax.set_xticklabels(ranks, fontproperties = zhfont)
+        ax.legend((rects1[0], rects2[0]), ('Men', 'Women'), fontsize = 'small')
+        def autolabel(rects):
+            for rect in rects:
+                height = rect.get_height()
+                ax.text(
+                    rect.get_x() + rect.get_width() / 2,
+                    1.05 * height,
+                    '%d' % int(height),
+                    ha = 'center',
+                    va='bottom',
+                )
+        autolabel(rects1)
+        autolabel(rects2)
+        fig.savefig(self.file_path + 'all1.jpg')
+        fig.clear()
+
+    def run(self):
+        while True:
+            print('*'*50, 'alive')
+            condition.acquire()
+            try:
+                if self.ip is not None:
+                    Statistics.items = self.stat_personal()
+                Statistics.age_bmi_count = self.stat_all_by_age()
+                self.stat_all_by_rank()
+                condition.wait()
+            except:
+                condition.wait()
+            condition.release()
 
 
-#各年龄段BMI平均值
-def stat_all_by_age():
-    filename = './commonstatic/statistics/all0.jpg'
-
-    #提取数据
-    bmi_by_age = []
-    sum_of_bmi = 0
-    count = 0
-    #提取各年龄段bmi和数与记录数量
-    for i in range(1, 151):
-        infos = Info.objects.filter(age = i)
-        count += len(infos)
-        if infos:
-            for info in infos:
-                sum_of_bmi += info.get_bmi()
-        else:
-            sum_of_bmi += 0
-        if not i%10:
-            bmi_by_age.append((i, sum_of_bmi, count))
-            sum_of_bmi = 0
-            count = 0
-    ages = [i for i in range(10, 151, 10)]
-    avebmi = [0 if i[1] == 0 else round(i[1]/i[2], 1) for i in bmi_by_age]
-
-    #构造图表
-    pl.figure('all0', figsize = (5.2, 2.8), dpi = 100)
-    pl.xlabel(u'年龄段', fontproperties = zhfont)
-    pl.ylabel('平均BMI', fontproperties = zhfont)
-    pl.ylim(0.0, 50.0)
-    pl.plot(ages, avebmi, '*', color = '#20b2aa')
-    pl.savefig(filename)
-    pl.cla()
-
-    avecount = [i[2] for i in bmi_by_age]
-    ages = ['%d-%d'%(i-9, i) for i in ages]
-    return list(zip(ages, avebmi, avecount))
+#单独一个线程绘图
+stat_thread = Statistics()
+stat_thread.start()
+condition = threading.Condition()
 
 
-#健康等级分布情况
-def stat_all_by_rank():
-    filename = './commonstatic/statistics/all1.jpg'
-    #pl.figure('all1', figsize = (5.2, 2.8), dpi = 100)
-
-    #提取数据
-    ranks = ['偏瘦', '正常', '偏胖', '肥胖', '重度肥胖', '极度肥胖']
-    men_infos = Info.objects.filter(gender = '男')
-    women_infos = Info.objects.filter(gender = '女')
-    men_ranks = [i.get_rank() for i in men_infos]
-    women_ranks = [i.get_rank() for i in women_infos]
-    men_count = [men_ranks.count(r) for r in ranks]
-    women_count = [women_ranks.count(r) for r in ranks]
-
-    #构造图表
-    ind = np.arange(6)
-    width = 0.35
-    fig, ax = pl.subplots(figsize = (5.2, 2.8))
-    rects1 = ax.bar(ind, men_count, width, color = '#20b2aa')
-    rects2 = ax.bar(ind + width, women_count, width, color = 'w')
-    ax.set_ylabel('数量', fontproperties = zhfont)
-    ax.set_xlim(-0.5, 7)
-    ax.set_xticks(ind+width)
-    ax.set_xticklabels(ranks, fontproperties = zhfont)
-    ax.legend((rects1[0], rects2[0]), ('Men', 'Women'), fontsize = 'small')
-    def autolabel(rects):
-        for rect in rects:
-            height = rect.get_height()
-            ax.text(
-                rect.get_x() + rect.get_width() / 2,
-                1.05 * height,
-                '%d' % int(height),
-                ha = 'center',
-                va='bottom',
-            )
-    autolabel(rects1)
-    autolabel(rects2)
-    fig.savefig(filename)
-    fig.clear()
-
-
-#站点数据视图
 def view_of_stat(request):
-    #获取数据信息
     ip = get_ip(request)
     ip_count = Ip.objects.count()
     items_count = Info.objects.count()
 
-    #获取三个图表
-    items = stat_personal(ip)
-    age_bmi_count = stat_all_by_age()
-    stat_all_by_rank()
-
     context = {
         'ip' : ip,
-        'items' : items,
+        'items' : Statistics.items,
         'ip_count' : ip_count,
         'items_count' : items_count,
-        'age_bmi_count' : age_bmi_count,
+        'age_bmi_count' : Statistics.age_bmi_count,
     }
 
     return render(request, 'statistics.html', context)
 
 
-#健康之家视图
 def view_of_health_home(request):
     return render(request, 'health_home.html')
