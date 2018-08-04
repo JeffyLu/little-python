0e6025bb80e3e876d94d5bc775ac326222063006
JeffyLu <a214121@126.com>
2018-08-04 10:54:27 +0800

    resolved #1

--- 0002/counter.py
+++ 0002/counter.py
@@ -1,5 +1,3 @@
-# coding:utf-8
-
 from collections import Counter
 
 
@@ -12,12 +10,13 @@ def main(filename):
         lines = context.count('\n')
         words = []
         for line in context.strip().split('\n'):
-            words += line.strip().split(' ')
+            words.extend([w for w in line.strip().split(' ') if w.isalpha()])
         counter = Counter(words)
         for k, v in counter.items():
             print('%s : %s' % (k, v))
         print('lines : %d' % lines)
         print('words : %d' % len(counter))
+
 
 if __name__ == '__main__':
 
